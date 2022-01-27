046b06247ccd44fe8046a85d36d4e06757edc8ca
Habib <habibdabbag@gmail.com>
2022-01-28 00:49:29 +0300

    new-project-with-docker

---
+++ project8/src/test/java/project8/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package project8.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
